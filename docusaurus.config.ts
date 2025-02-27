104d4514c3c4ec04292cc483cbadb2835274896a
resuadam2 <resuadam2@gmail.com>
2025-02-27 16:31:59 +0100

    starting with unity

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -110,6 +110,7 @@ const config: Config = {
     prism: {
       theme: prismThemes.github,
       darkTheme: prismThemes.dracula,
+      additionalLanguages: ['java', 'kotlin', 'csharp'],
     },
   } satisfies Preset.ThemeConfig,
 
